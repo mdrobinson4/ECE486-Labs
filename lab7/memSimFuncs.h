5a83dbecc937ea972cdb461e40145989d8cfad55
Your Name <mdrobinson4@crimson.ua.edu>
2019-04-13 17:31:36 -0500

    removed some unnecessary files

--- lab7/memSimFuncs.h
+++
@@ -1,8 +0,0 @@
-typedef struct memory MEMORY;
-MEMORY *newMEMORY(int count);
-void setCommand(MEMORY *m, char command, int index);
-MEMORY *readFile(FILE *fp);
-
-char getAssociativity();
-char getReplacementPolicy();
-FILE *getFile();
